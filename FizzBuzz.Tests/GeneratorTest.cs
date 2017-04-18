0c302454dd73d2b1250f3e815cbbf0df3a6cdb8d
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2017-04-18 06:54:04 +0100

    Basic Project structure

---
+++ FizzBuzz.Tests/GeneratorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class GeneratorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ FizzBuzz/Generator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Generator
+    {
+        IEnumerable<string> GetFuzBuzzOutput(int number)
+        {
+            yield return "";
+        }
+    }
+}
