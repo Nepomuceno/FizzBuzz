567668e96aa8027804aa005fa47d654492b21dd9
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2017-04-18 07:25:52 +0100

    Adding command line interface

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,23 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the bumber you want to print fizz buzz for:");
+            var value = Console.ReadLine();
+            if (int.TryParse(value, out int number))
+            {
+                Generator gen = new Generator();
+                var result = gen.GetFuzBuzzOutput(number);
+                foreach (var output in result)
+                {
+                    Console.Write($"{output} ");
+                }
+                Console.WriteLine();
+            } else
+            {
+                Console.WriteLine("You did not type a valid number");
+            }
+            Console.WriteLine("Press enter to finish");
+            Console.ReadLine();
         }
     }
 }
